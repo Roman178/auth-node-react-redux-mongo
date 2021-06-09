4f41b576d78a80386ea46eba6143add64886c5f4
Roman <ismagilov771@gmail.com>
2021-06-09 22:47:49 +0300

    Prepare to deploy

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const config = require("config");
 const mongoose = require("mongoose");
 const { authRouter } = require("./routes/auth.routes");
@@ -10,15 +11,28 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(cors());
+
 // Симуляция задержки ответа сервера
 // app.use(function (req, res, next) {
 //   setTimeout(next, 350);
 // });
 
-const PORT = config.get("port") || 5000;
+const PORT = process.env.PORT || config.get("port") || 5000;
 
 app.use("/api/auth", authRouter);
 app.use("/api", profileRouter);
+
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 mongoose
   .connect(config.get("mongoUri"), {
